a39d1bbb5e6f078b43456db79d9b7402b460e846
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-11-30 23:29:37 +0100

    13-Theorie: @Input (Kommunikation von parent zu child)

---
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-singlefruit',
+  standalone: true,
+  imports: [],
+  templateUrl: './singlefruit.component.html',
+  styleUrl: './singlefruit.component.scss'
+})
+export class SinglefruitComponent {
+
+  @Input("testNumber") fruitnumber = 0;
+
+}
